cce355a5be4e55e950d8a822e288eda3e35b95f9
Nordgaren <nordgarentv@gmail.com>
2024-01-19 17:56:52 -0700

    removed non template code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,2 @@
-pub use dll_proxy_derive::dll_proxy;
-use crate::winternals::LoadLibraryA;
+pub use proc_macro_template_derive::proc_macro;
 
-pub unsafe fn init_dll() -> bool {
-    let path = match crate::utils::get_dll_path_from_search_paths("") {
-        Some(p) => p,
-        None => return false,
-    };
-
-    let dll = LoadLibraryA(&path);
-    if dll == 0 {
-        return false;
-    }
-
-
-
-    true
-}
